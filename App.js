542a77b35d757b921a1886912a3972d3270c0a3a
Gabriel Hildebrandt <gabriel@signater.com>
2021-10-28 17:13:24 -0300

    custom user agent

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Dimensions } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <WebView
+        userAgent="Custom User Agent"
+        startInLoadingState={true}
+        style={{
+          marginTop: 20,
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height,
+        }}
+        source={{ uri: "https://google.com/" }}
+      />
     </View>
   );
 }
@@ -14,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
